dca1ac817b41cd25a906696f0a2f453a8264eeb7
Javier Prz <58307741+jvprz@users.noreply.github.com>
2023-09-11 11:09:14 +0200

    background component created

--- jvprz/src/app/app.module.ts
+++ jvprz/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
