a330d922ad83437bad8c2466a47bb6dd07aeed00
Javier Prz <58307741+jvprz@users.noreply.github.com>
2023-09-11 12:31:50 +0200

    landing component

--- jvprz/src/app/app.module.ts
+++ jvprz/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BackgroundComponent } from './background/background.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
