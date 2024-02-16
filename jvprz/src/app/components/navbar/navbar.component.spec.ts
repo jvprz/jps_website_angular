c32eb3e2c4a593891ebbdbe8954d0f1434239932
Javier Prz <58307741+jvprz@users.noreply.github.com>
2024-02-16 11:37:33 +0100

    navbar changes

--- jvprz/src/app/components/navbar/navbar.component.spec.ts
+++ jvprz/src/app/components/navbar/navbar.component.spec.ts
@@ -1,21 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => { // Añadido async para esperar renderizado
+    await TestBed.configureTestingModule({
       declarations: [NavbarComponent]
-    });
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); // Detecta cambios para renderizar la vista
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class Navbar implements OnInit { // Corregido el nombre a NavbarComponent
+
+  private toggleButton: HTMLElement | null = null; // Permitir valor null
+  private navLinks: HTMLElement | null = null;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.toggleButton = this.navbar.nativeElement.querySelector('.navbar-toggler') as HTMLElement; // Selector correcto y acceso a elementos
+    this.navLinks = this.navbar.nativeElement.querySelector('.navbar-nav') as HTMLElement;
+
+    this.toggleButton?.addEventListener('click', () => { // Encadenamiento opcional
+      this.navLinks?.classList.toggle('d-none');
+    });
+  }
+}
